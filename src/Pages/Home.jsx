bb53b5dc1073f962fa1dc8953208182ec2df48a1
PEACE-DFG <awofesobipeace@gmail.com>
2024-01-14 12:15:33 +0100

    styles and props

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import Index from '../component/Index'
 
 function Home() {
+
+  const user={
+    name: 'John',
+    age: 36,
+    Phone_Number:'48739567386473',
+    job:'Web developer'
+  }
   return (
     <div>
-      <Index/>
+      {/* <Index name={user.name} age={user.age}/> */}
+      <Index {...user}/>
     </div>
   )
 }
--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import './index.css';
 
-function Index() {
+import design from './styles.module.css'
+function Index(props) {
+// your js goes here
+ const styles={ 
+  color:'blue',
+  textAlign:'center',
+  border:'2px solid red',
+  backgroundColor:'grey'
+ }
+
+
   return (
     <div>
-      <h1>WELCOME TO REACT JS CLASS</h1>
+      <h1 style={styles}>WELCOME TO REACT JS CLASS</h1>
+      <div className='box'>
+
+      </div>
+      <div className={design.cent}>
+      <h1>Name:{props.name}</h1>
+      <h1>Age:{props.age}</h1>
+      <h1>Phone Number:{props.Phone_Number}</h1>
+      <h1>Job:{props.job}</h1>
+
+      </div>
     </div>
   )
 }
